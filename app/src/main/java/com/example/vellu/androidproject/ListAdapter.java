87f413b47b8171add500414c423b12fafe495473
VelluVu <velimavu@metropolia.fi>
2018-06-04 23:29:19 +0300

    Exercise two "fixed text inputs for activity four", and created "calculator buttons to complete Exercise Three".

---
+++ app/src/main/java/com/example/vellu/androidproject/ListAdapter.java
@@ -0,0 +1,35 @@
+package com.example.vellu.androidproject;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.widget.ArrayAdapter;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class ListAdapter extends ArrayAdapter {
+
+    HashMap<Object, Integer> mIdMap;
+
+
+    public ListAdapter(@NonNull Context context, int resource, @NonNull List objects) {
+        super(context, resource, objects);
+
+        mIdMap = new HashMap<>();
+
+        for (int i = 0; i < objects.size() ; ++i) {
+            mIdMap.put(objects.get(i), i);
+        }
+    }
+
+    @Override
+    public long getItemId(int position) {
+        Object item = getItem(position);
+        return mIdMap.get(item);
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+}
