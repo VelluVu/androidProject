712a287cd91677489cec9afeb3371f58e7da496f
VelluVu <velimavu@metropolia.fi>
2018-06-06 12:16:52 +0300

    all exercises completed

--- app/src/main/java/com/example/vellu/androidproject/MainActivity.java
+++ app/src/main/java/com/example/vellu/androidproject/MainActivity.java
@@ -4,16 +4,18 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.nio.file.ClosedDirectoryStreamException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,12 +25,41 @@ public class MainActivity extends AppCompatActivity {
     EditText typeHereTwo;
     TextView alertText;
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity","Starting");
+    }
 
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("MainActivity", "Resuming");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("MainActivity", "Restarting");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity", "Destroying");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "Stopping");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Creating");
 
         Toast.makeText(MainActivity.this, R.string.thisismain, Toast.LENGTH_SHORT).show();
 
@@ -129,13 +160,44 @@ public class MainActivity extends AppCompatActivity {
                 public void onClick(DialogInterface dialog, int which) {
 
                     alertText.setVisibility(View.VISIBLE);
-                    finish();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                        finishAffinity();
+                    }
                     System.exit(0);
+                    finish();
                 }
             });
             builder.show();
 
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int op = item.getItemId();
+        switch (op) {
+
+            case R.id.something1:
+                Toast.makeText(MainActivity.this, R.string.youclicksome1, Toast.LENGTH_SHORT).show();
+                Log.d("MainActivity" , "You are at switch case something 1");
+                return true;
+            case R.id.something2:
+                Toast.makeText(MainActivity.this, R.string.youclicksome2, Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.something3:
+                Toast.makeText(MainActivity.this, R.string.youclicksome3, Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.something4:
+                Toast.makeText(MainActivity.this, R.string.youclicksome4, Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
